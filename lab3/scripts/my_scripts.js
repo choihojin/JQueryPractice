e4e6393770ea3c6da78b339792c585981ed653c9
hojin <inanuri7@naver.com>
2016-01-27 16:20:20 +0900

    JQueryDivMotion
    구간별 나눠진 모션 체크

---
+++ lab3/scripts/my_scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $(".guess_box").click(checkForCode);
+    function getRandom(num) {
+        var my_num = Math.floor(Math.random() * num);
+        return my_num;
+    }
+    var hideCode = function () {
+        var numRand = getRandom(4);
+        $(".guess_box").each(function (index, value) {
+            if (numRand == index) {
+                $(this).append("<span id='has_discount'></span>")
+                return false;
+            }
+        });
+    }
+   
+    hideCode();
+   
+    function checkForCode() {
+        var discount;
+        if ($.contains(this, document.getElementById("has_discount"))) {
+            var my_num = getRandom(100);
+            discount = "<p>Your Code: CODE" + my_num + "</p>";
+        }
+        else {
+            discount = "<p>Sorry, no discount this time!</p>";
+        }
+        $(this).append(discount);
+        $(".guess_box").each(function () {
+            if ($.contains(this, document.getElementById("has_discount"))) {
+                $(this).addClass("discount");
+            } else {
+                $(this).addClass("no_discount");
+            }
+            $(this).unbind();
+        });
+        $("#result").append(discount);
+    }
+});
